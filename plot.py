5627d4f3cfade83e70b5830eb4b119400485a252
metametaclass <metametaclass@gmail.com>
2022-12-25 11:49:36 +0100

    #3389 init figure settings

--- plot.py
+++ plot.py
@@ -7,6 +7,17 @@ import matplotlib.pyplot as plt
 import json
 import pandas as pd
 
+def init_figure_settings(width=12, height=5):
+    plt.rcParams["figure.figsize"] = [width, height]
+    plt.rcParams['font.size'] = 14
+    plt.rcParams['image.cmap'] = 'plasma'
+    plt.rcParams['axes.linewidth'] = 2
+    # Set the default colour cycle (in case someone changes it...)
+    from cycler import cycler
+    cols = plt.get_cmap('tab10').colors
+    plt.rcParams['axes.prop_cycle'] = cycler(color=cols)
+
+
 def _parse_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--plot", action="store_true", help="show plots")
@@ -24,8 +35,8 @@ def _process_file(fn, show_plot):
         data = pd.read_json(f, lines=True)
         sensors = pd.crosstab(data.time, data.id, data.temperature_F, aggfunc=min)
         # sensors = pd.pivot_table(data, index=["time", "id"])
-        print(data)
-        print(sensors)
+        # print(data)
+        # print(sensors)
         print(data.keys())
         print(sensors.keys())
 
@@ -48,11 +59,13 @@ def _handle_stream(f):
 
 if __name__ == "__main__":
     args = _parse_arguments()
+    if args.plots:
+        init_figure_settings()
     if args.stream:
         _handle_stream(fileinput.input(args.files))
     else:
         for file in args.files:
             _process_file(file, args.plot)
     if args.plot:
-        plt.get_current_fig_manager().full_screen_toggle()
+        # plt.get_current_fig_manager().full_screen_toggle()
         plt.show()
